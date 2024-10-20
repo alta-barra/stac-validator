43e9e381ead414ddc645facbb7b939b9858129cc
Jay Barra <jay@alta-barra.com>
2024-10-20 11:43:35 -0400

    Initial commit

---
+++ lib/stac_validator.ex
@@ -0,0 +1,18 @@
+defmodule StacValidator do
+  @moduledoc """
+  Documentation for `StacValidator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StacValidator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stac_validator_test.exs
@@ -0,0 +1,8 @@
+defmodule StacValidatorTest do
+  use ExUnit.Case
+  doctest StacValidator
+
+  test "greets the world" do
+    assert StacValidator.hello() == :world
+  end
+end
